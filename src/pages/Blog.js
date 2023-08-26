9908785747b93bbc00e61a1c9e3f21b204501ffc
Okaye Hako <dimitribpusch21@gmail.com>
2023-08-26 08:36:16 -0300

    fix(Blog) document title

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,34 +1,36 @@
-import {useState, useEffect} from 'react';
-import Header from '../components/Header';
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
 
 const Blog = () => {
-    const [post, setPost] = useState([]);
-    useEffect(()=>{
-        fetch("https://opomboapi.vercel.app/app/index.json")
-        .then(res=>res.json())
-        .then(dt=>{
-            dt['blog'][0]['id']===0?setPost(dt['blog']):setPost(dt['blog'].reverse())
-        })
-    })
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    fetch("https://opomboapi.vercel.app/app/index.json")
+      .then((res) => res.json())
+      .then((dt) => {
+        dt["blog"][0]["id"] === 0
+          ? setPost(dt["blog"])
+          : setPost(dt["blog"].reverse());
+      });
+  });
+
+  document.title = "Blog - O Pombo Jornal";
   return (
     <main>
-        <Header/>
-        <div className='resp-container'>
-            <h1>Blog</h1>
-        </div>
-        <div className='card-list'>
-            {
-                post.map(p=>(
-                    <div className='card card-author'>
-                        <h3>{p.title}</h3>
-                        <p>{p.content}</p>
-                        <i style={{marginTop: 10, fontSize: 15}}>{p.date}</i>
-                    </div>
-                ))
-            }
-        </div>
+      <Header />
+      <div className="resp-container">
+        <h1>Blog</h1>
+      </div>
+      <div className="card-list">
+        {post.map((p) => (
+          <div className="card card-author">
+            <h3>{p.title}</h3>
+            <p>{p.content}</p>
+            <i style={{ marginTop: 10, fontSize: 15 }}>{p.date}</i>
+          </div>
+        ))}
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
